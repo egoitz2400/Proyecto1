396b618a0836854225d1bf0e47fc8c7a051d8922
egoitz <egoitz@hotmail.com>
2022-01-24 08:54:29 +0100

    Cambios en clase Principal Rama cambio

--- proyecto1/src/prueba1/Principal.java
+++ proyecto1/src/prueba1/Principal.java
@@ -4,5 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo desde java-git");
 		System.out.println("Adios");
+		System.out.println("Clase Principal");
+		System.out.println("Pruebas Git. Clase Principal");
 	}
 }
